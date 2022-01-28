e4b6746a8ef1c62cfcfd56394d7628806464ed52
Prashanth-bafna <prasanthbafna@gmail.com>
2022-01-28 18:45:29 +0530

    First Commit

---
+++ learning_users/new_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from new_app import views
+
+app_name='new_app'
+
+urlpatterns=[
+    path('register/',views.register,name='register'),
+    path('user_login/',views.user_login,name='user_login'),
+]
